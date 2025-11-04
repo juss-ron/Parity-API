335f085c3ee8527caf9079c67932aa9a1b210a41
Ron <ronshadreck@gmail.com>
2025-11-04 09:11:58 +0200

    Created a transaction model

---
+++ models/Transaction.js
@@ -0,0 +1,43 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database');
+
+const Transaction = sequelize.define('Transaction', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        unique: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    memberId: {
+        type: DataTypes.UUID,
+        allowNull: false
+    },
+    clubId: {
+        type: DataTypes.UUID,
+        allowNull: false
+    },
+
+    investAmount: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+        allowNull: false
+    },
+    interestAmount: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+        allowNull: false
+    },
+    payLoanAmount: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+        allowNull: false
+    },
+    loanAmount: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+        allowNull: false
+    },
+});
+
+module.exports = Transaction
