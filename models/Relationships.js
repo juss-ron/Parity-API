fda643be0c429961dede12faf3a9c64d56726c13
Ron <ronshadreck@gmail.com>
2025-11-04 09:14:17 +0200

    Defined the relationship between members and transactions

--- models/Relationships.js
+++ models/Relationships.js
@@ -1,6 +1,7 @@
 const Club = require('./Club');
 const Member = require('./Member');
 const User = require('./User');
+const Transaction = require('./Transaction');
 
 User.hasMany(Club, { as: 'clubs', foreignKey: 'userId' });
 Club.belongsTo(User, { as: 'owner', foreignKey: 'userId' });
@@ -11,4 +12,7 @@ Member.belongsTo(User, { as: 'user', foreignKey: 'userId' });
 Club.hasMany(Member, { as: 'members', foreignKey: 'clubId' });
 Member.belongsTo(Club, { as: 'club', foreignKey: 'clubId' });
 
-module.exports = { Club, Member, User }
+Member.hasMany(Transaction, { as: 'transaction', foreignKey: 'memberId' });
+Transaction.belongsTo(Member, { as: 'member', foreignKey: 'memberId' });
+
+module.exports = { Club, Member, User, Transaction }
